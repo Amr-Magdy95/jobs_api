932f5d8b36578218cf9b9b3fc253406ed65d3049
Amr-Magdy95 <37376273+Amr-Magdy95@users.noreply.github.com>
2023-11-27 10:01:34 +0200

    Feature: Added security packages

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ const express = require("express");
 const app = express();
 
 // middleware
+app.set("trust proxy", 1);
+app.use(
+  require("express-rate-limit")({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
 app.use(express.json());
+app.use(require("helmet")());
+app.use(require("cors")());
+app.use(require("xss-clean")());
 
 // routes
 app.get("/", (req, res) => {
