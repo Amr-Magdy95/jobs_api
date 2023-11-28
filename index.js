55a3b0ac5f06092bdc9a5c9373fed9e1ecd5c9b2
Amr-Magdy95 <37376273+Amr-Magdy95@users.noreply.github.com>
2023-11-28 08:28:03 +0200

    Prepared for deployment on Adaptable io

--- index.js
+++ index.js
@@ -37,7 +37,9 @@ const port = process.env.PORT || 5000;
 const start = async () => {
   try {
     console.log("Connecting to the DB...");
-    await require("mongoose").connect(process.env.MONGO_URI);
+    await require("mongoose").connect(
+      process.env.MONGO_URI || process.env.DATABASE_URL
+    );
     console.log("Connected to the DB...");
     app.listen(port, console.log(`Server is listening on port: ${port}...`));
   } catch (err) {
